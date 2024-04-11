41b80aa6e4564fb4e3a557e436bf59a075b3721f
Omar Campos Rivera <ocampos@invidgroup.com>
2024-04-11 15:53:25 -0400

    Initial commit

---
+++ src/main/java/com/mentalshard/azureauth/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.mentalshard.azureauth.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello/{name}")
+    public String hello(@PathVariable String name){
+        return "Hello " + name;
+    }
+}
